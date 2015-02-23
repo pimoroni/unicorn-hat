7f6f6f281134663ac84f46ce9651a28366680c7b
lowfatcode <jon@magicnumber.co.uk>
2015-02-23 19:15:51 +0000

    Fixed to be more rainbow-y

--- python/examples/rainbow.py
+++ python/examples/rainbow.py
@@ -3,19 +3,22 @@
 import unicornhat as unicorn
 import time, colorsys
 
-print("Some LEDs wont light up in this example.")
-print("Don't panic!")
+print("Reticulating splines")
+time.sleep(.5)
+print("Enabled unicorn poop module!")
+time.sleep(.5)
+print("Pooping rainbows...")
 
 unicorn.brightness(0.9)
 
 while True:
-	for z in range(360):
-		for y in range(8):
-			for x in range(8):
-				rgb = colorsys.hsv_to_rgb(z/360.0,y/7.0,x/7.0)
-				r = int(rgb[0]*255.0)
-				g = int(rgb[1]*255.0)
-				b = int(rgb[2]*255.0)
-				unicorn.set_pixel(x,y,r,g,b)
-		unicorn.show()
-		time.sleep(0.1)
+        for z in range(360):
+                for y in range(8):
+                        for x in range(8):
+                                rgb = colorsys.hsv_to_rgb(z/360.0,y/7.0,x/7.0)
+                                r = x * 32
+                                g = y * 32
+                                b = 255 - (x * 32)
+                                unicorn.set_pixel(x,y,int(r),int(g),int(b))
+                unicorn.show()
+                time.sleep(0.1)
