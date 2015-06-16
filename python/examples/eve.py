05ba3b3972dd18068e18eed3fbc06485b0e55ef7
Jonathan Williamson <jon@pimoroni.com>
2015-06-16 07:31:14 +0000

    Eve demo

---
+++ python/examples/eve.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import unicornhat as unicorn
+import time, math, colorsys, random
+
+sin_off = [[0]*8 for i in range(8)]
+for y in range(8):
+    for x in range(8):
+        sin_off[x][y] = random.random() * (math.pi * 2)
+
+tick_mask = [[0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,1,0],
+             [0,0,0,0,0,1,1,1],
+             [0,0,1,0,1,1,1,0],
+             [0,1,1,1,1,1,0,0],
+             [0,0,1,1,1,0,0,0],
+             [0,0,0,1,0,0,0,0],
+             [0,0,0,0,0,0,0,0]]
+
+steps_per = 16
+def background(x, y, step):
+    v = math.sin(sin_off[x][y] + (step / 10.0))
+
+    g = v + 1.0
+    g = g * 20.0
+    g += 20
+
+    v = math.sin(sin_off[y][x] + (step / 20.0))
+
+    b = v + 1.0
+    b = b * 15.0
+    b += 20      
+
+    r = 0
+   
+    g /= (r/50) + 1
+    b /= (r/50) + 1
+
+    pos = int(step / steps_per)
+    fill = int((float(step % steps_per) / float(steps_per)) * 8.0)
+    print fill
+    if x < pos:
+        r = math.sin(sin_off[y][x] + (step / 20.0))
+        r *= 30
+        r += 80
+        g = 0
+        b = 0
+    elif x == pos and y < fill:
+        r = math.sin(sin_off[y][x] + (step / 20.0))
+        r *= 30
+        r += 80
+        g = 0
+        b = 0
+
+    return (g, r, b)
+
+unicorn.brightness(1)
+
+for i in range(steps_per * 8):
+    for y in range(8):
+        for x in range(8):              
+            r, g, b = background(x, y, i)
+            r = int(max(0, min(255, r)))
+            g = int(max(0, min(255, g)))
+            b = int(max(0, min(255, b)))
+            unicorn.set_pixel(x, y, r, g, b)
+
+    unicorn.show()
+
+    time.sleep(0.01)
+
+for i in range(200):
+    v = (math.sin(i / 6.0) + 1.0) / 2.0
+    for y in range(8):
+        for x in range(8):
+            r = 0
+            b = 0             
+            g = 100
+            g *= tick_mask[y][x]
+            g *= v
+            r = int(max(0, min(255, r)))
+            g = int(max(0, min(255, g)))
+            b = int(max(0, min(255, b)))
+            unicorn.set_pixel(x, y, r, g, b)
+
+    unicorn.show()
+
+    time.sleep(0.02)
