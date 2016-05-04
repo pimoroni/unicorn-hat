8d17b85438f5cf3261568b107e7547401fb562e9
Phil Howard <phil@gadgetoid.com>
2016-05-04 11:10:00 +0000

    Bumped version for upload

--- python/rpi-ws281x/setup.py
+++ python/rpi-ws281x/setup.py
@@ -13,7 +13,7 @@ class CustomInstallCommand(build_py):
         build_py.run(self)
 
 setup(name              = 'rpi_ws281x',
-      version           = '2.0.0',
+      version           = '2.0.1',
       author            = 'Jeremy Garff',
       author_email      = 'jer@jers.net',
       description       = 'Userspace Raspberry Pi PWM library for WS281X LEDs.',
