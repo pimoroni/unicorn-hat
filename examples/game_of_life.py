34d8b64520ef5baa65f1bfeea9b7955be508d5ec
David Glaude <david.glaude@gmail.com>
2016-07-12 23:14:30 +0200

    Tentative adaptation of the game of live to pHAT or any geometry. Should work with any width and height.
    This can be tested with Unicorn HAT and adding
    
    width=width-1
    height=height-2
    
    before the line
    
    size=width*height

--- examples/game_of_life.py
+++ examples/game_of_life.py
@@ -4,15 +4,20 @@ import unicornhat as unicorn
 import random
 import time
 
+unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(90)
+unicorn.brightness(0.4)
+width,height=unicorn.get_shape()
+
+size = width*height
 
 class GameOfLife:
     def __init__(self):
-        self.board = [int(7 * random.getrandbits(1)) for _ in xrange(64)]
+        self.board = [int(7 * random.getrandbits(1)) for _ in xrange(size)]
 	self.color = [[154, 154, 174], [0, 0, 255], [0, 0, 200], [0, 0, 160], [0, 0, 140], [0, 0, 90], [0, 0, 60], [0, 0, 0,]]
 
     def value(self, x, y):
-        index = ((x % 8) * 8) + (y % 8)
+        index = ((x % width) * height) + (y % height)
         return self.board[index]
 
     def neighbors(self, x, y):
@@ -26,34 +31,34 @@ class GameOfLife:
         return sum
 
     def next_generation(self):
-        new_board = [False] * 64
-        for i in xrange(8):
-            for j in xrange(8):
+        new_board = [False] * size
+        for i in xrange(width):
+            for j in xrange(height):
                 neigh = self.neighbors(i, j)
                 lvl = self.value(i, j)
                 if lvl == 0:
                     if neigh < 2:
-                        new_board[i * 8 + j] = min(7, lvl + 1)
+                        new_board[i * height + j] = min(7, lvl + 1)
                     elif 2 <= neigh <= 3:
-                        new_board[i * 8 + j] = 0
+                        new_board[i * height + j] = 0
                     else:
-                        new_board[i * 8 + j] = min(7, lvl + 1)
+                        new_board[i * height + j] = min(7, lvl + 1)
                 else:
                     if neigh == 3:
-                        new_board[i * 8 + j] = 0
+                        new_board[i * height + j] = 0
                     else:
-                        new_board[i * 8 + j] = min(7, lvl + 1)
+                        new_board[i * height + j] = min(7, lvl + 1)
         self.board = new_board
 
     def all_dead(self):
-        for i in xrange(64):
+        for i in xrange(size):
             if self.board[i] != 7:
                 return False
         return True
 
     def show_board(self):
-        for i in xrange(8):
-            for j in xrange(8):
+        for i in xrange(width):
+            for j in xrange(height):
                rgb = self.color[self.value(i, j)]
                unicorn.set_pixel(i, j, rgb[0], rgb[1], rgb[2])
         unicorn.show()
