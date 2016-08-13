fd119287fd4d913d313a5d2e70972a023e4452d2
Joshua Arnott <josh@snorfalorpagus.net>
2016-08-13 10:19:11 +0000

    Support PWM1 in unicornhat.py

--- library/UnicornHat/unicornhat.py
+++ library/UnicornHat/unicornhat.py
@@ -10,9 +10,13 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
+LED_CHANNEL    = 0       # PWM channel
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
-ws2812 = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+# For PWM0 (default setup) use: CHANNEL 0, PIN 18, DMA 5
+# For PWM1 (requires re-soldering) use: CHANNEL 1, PIN 13, DMA 8
+
+ws2812 = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 ws2812.begin()
 
 """
