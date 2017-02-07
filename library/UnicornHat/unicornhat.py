9f4913b5bfd918f34f676727986a53daa5115748
Phil Howard <phil@gadgetoid.com>
2017-02-07 13:33:49 +0000

    Updated to use new rpi_ws281x lib

--- library/UnicornHat/unicornhat.py
+++ library/UnicornHat/unicornhat.py
@@ -1,7 +1,11 @@
 import atexit
 import colorsys
 
-from neopixel import Adafruit_NeoPixel
+try:
+    from rpi_ws281x import __version__, PixelStrip, Color
+except ImportError:
+    from neopixel import Adafruit_NeoPixel as PixelStrip, Color
+    __version__ = "legacy"
 
 
 # LED strip configuration:
@@ -13,7 +17,7 @@ LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
 LED_CHANNEL    = 0       # PWM channel
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
-ws2812 = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+ws2812 = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 ws2812.begin()
 
 """
@@ -60,6 +64,27 @@ PHAT = [
 ]
 
 AUTO = None
+
+GAMMA = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11,
+11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18,
+19, 19, 20, 21, 21, 22, 22, 23, 23, 24, 25, 25, 26, 27, 27, 28,
+29, 29, 30, 31, 31, 32, 33, 34, 34, 35, 36, 37, 37, 38, 39, 40,
+40, 41, 42, 43, 44, 45, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+71, 72, 73, 74, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 88, 89,
+90, 91, 93, 94, 95, 96, 98, 99,100,102,103,104,106,107,109,110,
+111,113,114,116,117,119,120,121,123,124,126,128,129,131,132,134,
+135,137,138,140,142,143,145,146,148,150,151,153,155,157,158,160,
+162,163,165,167,169,170,172,174,176,178,179,181,183,185,187,189,
+191,193,194,196,198,200,202,204,206,208,210,212,214,216,218,220,
+222,224,227,229,231,233,235,237,239,241,244,246,248,250,252,255]
+
+if __version__ == "legacy":
+    GAMMA = [x for x in range(256)]
 
 def set_layout(pixel_map = AUTO):
     """Set the layout to Unicorn HAT or Unicorn pHAT
@@ -196,10 +221,8 @@ def set_pixel_hsv(x, y, h, s, v):
     :param s: Saturation from 0.0 to 1.0
     :param v: Value (also known as brightness) from 0.0 to 1.0
     """
-    index = get_index_from_xy(x, y)
-    if index is not None:
-        r, g, b = [int(n*255) for n in colorsys.hsv_to_rgb(h, s, v)]
-        ws2812.setPixelColorRGB(index, r, g, b)
+    r, g, b = [int(n*255) for n in colorsys.hsv_to_rgb(h, s, v)]
+    set_pixel(x, y, r, g, b)
 
 
 def set_pixel(x, y, r, g, b):
@@ -213,6 +236,7 @@ def set_pixel(x, y, r, g, b):
     """
     index = get_index_from_xy(x, y)
     if index is not None:
+        r, g, b = GAMMA[r], GAMMA[g], GAMMA[b]
         ws2812.setPixelColorRGB(index, r, g, b)
 
 
