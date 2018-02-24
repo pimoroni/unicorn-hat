00095e2596e73e469408a7f0e64a7fdea1de1d06
IDF31 <iorgadragos04@gmail.com>
2018-02-24 19:29:36 +0200

    Update unicornhat.py

--- library/UnicornHat/unicornhat.py
+++ library/UnicornHat/unicornhat.py
@@ -326,7 +326,7 @@ def set_pixel(x, y, r, g=None, b=None):
             r, g, b = COLORS[r.lower()]
         
         except KeyError:
-            r, g, b = int(r,16).to_bytes(3,'big')
+            r, g, b = int(r.lstrip('#'),16).to_bytes(3,'big')
 
     index = get_index_from_xy(x, y)
 
