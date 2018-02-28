9bc5f16f4aeeaa5529015aa4b774f89b736fc09e
Iorga Dragos Florian <iorgadragos04@gmail.com>
2018-02-28 19:01:59 +0200

    Update binaryclock.py

--- examples/hat/binaryclock.py
+++ examples/hat/binaryclock.py
@@ -1,35 +1,39 @@
+#!/usr/bin/env python
 import unicornhat as uh
 import datetime
+from time import sleep
 
+print("""Binar Clock made by Iorga Dragos Florian
+Displays Hour(red), Minute(yellow), Second(blue), Day(green) and Month(Pink) in binary format""")
+FPS = 30.0
 uh.rotation(0)
 uh.brightness(1)
 
 def draw_time(b,x,idx,color):
     if b == '1':
         uh.set_pixel(x,idx,color)
-
+    sleep(1.0/FPS)
 
 while True:
     now = datetime.datetime.now()
-    an = bin(now.month)[2:][::-1]
-    luna = bin(now.month)[2:][::-1]
-    zi = bin(now.day)[2:][::-1]
-    ora = bin(now.hour)[2:][::-1]
-    minut = bin(now.minute)[2:][::-1]
-    secunda = bin(now.second)[2:][::-1]
+    month = bin(now.month)[2:][::-1]
+    day = bin(now.day)[2:][::-1]
+    hour = bin(now.hour)[2:][::-1]
+    minute = bin(now.minute)[2:][::-1]
+    second = bin(now.second)[2:][::-1]
     uh.clear()
-    for idx,b in enumerate(ora):
+    for idx,b in enumerate(hour):
         draw_time(b,7,idx,'red')
 
-    for idx,b in enumerate(minut):
+    for idx,b in enumerate(minute):
         draw_time(b,6,idx,'yellow')
 
-    for idx,b in enumerate(secunda):
+    for idx,b in enumerate(second):
         draw_time(b,5,idx,'blue')
 
-    for idx,b in enumerate(zi):
+    for idx,b in enumerate(day):
         draw_time(b,3,idx,'green')
 
-    for idx,b in enumerate(luna):
+    for idx,b in enumerate(month):
         draw_time(b,2,idx,'purple')
     uh.show()
